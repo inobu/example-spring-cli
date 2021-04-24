ccbeb264dd54453608b10085c8a72fa97a29d6b2
inobu <inohkunobuya@gmail.com>
2021-04-24 22:17:50 +0900

    とりあえず側だけ作った

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+}
