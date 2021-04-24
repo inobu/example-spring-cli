04a35acc7375f35eb5559b18c3c505f5642cacf0
inobu <inohkunobuya@gmail.com>
2021-04-24 23:34:39 +0900

    Runnerクラスだけ用意した

---
+++ src/main/kotlin/example/spring/cli/runner/ApplicationRunnerImpl.kt
@@ -0,0 +1,22 @@
+package example.spring.cli.runner
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class ApplicationRunnerImpl: ApplicationRunner {
+
+    override fun run(args: ApplicationArguments?) {
+        println("値を入力してください")
+
+        //対話開始
+        val line = readLine()
+        repeat((1..10).count()) {
+            println(line)
+        }
+
+        //コマンド値とか取れるはず
+        args?.sourceArgs
+    }
+}
