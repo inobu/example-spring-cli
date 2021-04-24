7cc8a4362edcc91417d0896e73af42b14d103c01
inobu <inohkunobuya@gmail.com>
2021-04-24 22:33:08 +0900

    依存関係増やした

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    kotlin("kapt") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
     id("java")
+    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
 }
 
 repositories {
@@ -9,4 +13,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
